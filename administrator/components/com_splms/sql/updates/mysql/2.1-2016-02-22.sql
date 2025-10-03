ed753d2473fddb7fe8183283adda77b0bf93c2c1
johnnymgo <johnnymgo@gmail.com>
2025-10-03 17:32:06 -0300

    Atualizacoes

--- administrator/components/com_splms/sql/updates/mysql/2.1-2016-02-22.sql
+++
@@ -1,9 +0,0 @@
-
---
--- Updated 2.0.4
---
-
--- ALTER TABLES;
-ALTER TABLE `#__splms_certificates` ADD issue_date date DEFAULT NULL;
-ALTER TABLE `#__splms_quizquestions` ADD quiz_type tinyint(1) DEFAULT '0';
-
